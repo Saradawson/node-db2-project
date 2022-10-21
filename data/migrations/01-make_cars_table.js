243686af90112219ed0922eb512ef7cf0eff97ba
unknown <saradawsone@gmail.com>
2022-10-21 17:22:49 -0600

    table migrations created

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
-exports.up = function (knex) {
-  
+exports.up = async function (knex) {
+  await knex.schema.createTable('cars', table => {
+    table.increments('id')
+    table.string('vin', 13).notNulable().unique()
+    table.string('make', 128).notNulable()
+    table.string('model', 128).notNulable()
+    table.numeric('mileage').unsigned().notNulable()
+    table.string('title', 128)
+    table.string('transmission', 128)
+  })
 };
 
-exports.down = function (knex) {
+exports.down = async function (knex) {
   // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists('cars')
 };
