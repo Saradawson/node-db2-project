cd58eefac8ce2c193bef1259c56000e236e9d0e5
unknown <saradawsone@gmail.com>
2022-10-21 17:08:20 -0600

    seed created

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,30 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        modle: 'prius',
+        milage: 12234,
+        title: 'clean',
+        transmission: 'manual'
+    },
+    {
+        vin: '1111111111111',
+        make: 'toyota',
+        modle: 'corolla',
+        milage: 150000,
+        title: 'salvaged',
+    },
+    {
+        vin: '1111111111111',
+        make: 'ford',
+        modle: 'focus',
+        milage: 122334
+    },
+
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
