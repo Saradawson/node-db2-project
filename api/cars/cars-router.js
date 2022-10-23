cc609754fca18652aff872d838d97686ce77387f
unknown <saradawsone@gmail.com>
2022-10-23 17:22:50 -0600

    car router endpoints created

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,31 @@
-// DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+   try{
+        res.json('router get')
+   }catch(err) {
+        next(err)
+   }
+})
+
+router.get('/:id', async (req, res, next) => {
+    try{
+        res.json('router get id')
+    }catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try{
+        res.json('router post')
+    }catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router
+
+
